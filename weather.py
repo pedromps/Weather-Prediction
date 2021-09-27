f273735f5b94766cef73dc84a70207d5aa985392
pedromps <90725927+pedromps@users.noreply.github.com>
2021-09-27 15:09:51 +0100

    Update weather.py
    sequence prediction with LSTM, still to be fine tuned

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, TimeDistributed
 
 # code adapted from F. Chollet's book Deep Learning With Python
 fname = 'jena_climate_2009_2016.csv'
@@ -19,59 +19,68 @@ for i, line in enumerate(lines):
     values = [float(x) for x in line.split(',')[1:]]
     float_data[i, :] = values
     
-    
-mean = float_data[:200000].mean(axis = 0)
-float_data -= mean
-std = float_data[:200000].std(axis = 0)
-float_data /= std
+norm_max = np.nanmax(float_data, axis = 0)
+norm_min = np.nanmin(float_data, axis = 0)
+float_data = (float_data-norm_min)/(norm_max-norm_min)
+
 
 lookback = 720 # observations look back in 10 days
 step = 6 # one data point per hour
 delay = 144 # 24h
 batch_size = 128
 
+# temperature is the target
+target = float_data[:, 2]
+float_data = np.delete(float_data, 2, axis=1)
 
-def train_test_val_split(data, lookback, step):
+def train_test_val_split(data, y, lookback, step):
     # training data will be derived from the first sequences, validation from the 
     # sequences following them and test following validation
     size = len(data)
-    train_size = [0, int(0.7*size)-int(0.7*size)%lookback] #<- 408 sequences
-    val_size = [train_size[1], train_size[1]+int(0.2*size)-int(0.2*size)%lookback]
+    train_size = [0, int(0.8*size)-int(0.8*size)%lookback] #<- 408 sequences
+    val_size = [train_size[1], train_size[1]+int(0.1*size)-int(0.1*size)%lookback]
     test_size = [val_size[1], val_size[1]+int(0.1*size)-int(0.1*size)%lookback]
     
     
     st = train_size[1]-train_size[0]
     aux = np.copy(data[train_size[0]:train_size[1]])
-    train = np.reshape(aux[::step], (int(st/lookback), int(lookback/step), data.shape[1]))
+    aux_y = np.copy(y[train_size[0]:train_size[1]])
+    x_train = np.reshape(aux[::step], (int(st/lookback), int(lookback/step), data.shape[1]))
+    y_train = np.reshape(aux_y[::step], (int(st/lookback), int(lookback/step), 1))
     
     sv = val_size[1]-val_size[0]
     aux = np.copy(data[val_size[0]:val_size[1]])
-    val = np.reshape(aux[::step], (int(sv/lookback), int(lookback/step), data.shape[1]))
+    aux_y = np.copy(y[val_size[0]:val_size[1]])
+    x_val = np.reshape(aux[::step], (int(sv/lookback), int(lookback/step), data.shape[1]))
+    y_val = np.reshape(aux_y[::step], (int(sv/lookback), int(lookback/step), 1))
     
     stt = test_size[1]-test_size[0]
     aux = np.copy(data[test_size[0]:test_size[1]])
-    test = np.reshape(aux[::step], (int(stt/lookback), int(lookback/step), data.shape[1]))
-
+    aux_y = np.copy(y[test_size[0]:test_size[1]])
+    x_test = np.reshape(aux[::step], (int(stt/lookback), int(lookback/step), data.shape[1]))
+    y_test = np.reshape(aux_y[::step], (int(stt/lookback), int(lookback/step), 1))
     
-    return train, test, val
+    return x_train, x_test, x_val, y_train, y_test, y_val
 
 
-train, test, val = train_test_val_split(float_data, lookback, step)
+x_train, x_test, x_val, y_train, y_test, y_val = train_test_val_split(float_data, target, lookback, step)
 
 
 model = Sequential()
-model.add(LSTM(32, activation = 'relu', input_shape = (train.shape[1], train.shape[2])))
-model.add(Dense(1, activation = 'relu'))
-model.compile(optimizer = "adam", loss = 'mae')
+model.add(LSTM(128, activation = 'relu', input_shape = (x_train.shape[1], x_train.shape[2])))
+# model.add(TimeDistributed(Dense(train.shape[1], activation = 'relu')))
+model.add(Dense(x_train.shape[1]))
+model.compile(optimizer = "adam", loss = 'mse')
 model.summary()
-history = model.fit(train, epochs = 20, verbose = 1)#, validation_data = val)
+history = model.fit(x_train, y_train, epochs = 40, verbose = 1, batch_size = 256, validation_data = (x_val, y_val))
 
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-epochs = range(1, len(loss) + 1)
 plt.figure()
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.plot(history.history['loss'], label='Training loss')
+plt.plot(history.history['val_loss'], label='Validation loss')
 plt.title('Training and validation loss')
 plt.legend()
 plt.show()
+
+temp_pred = model.predict(x_test)
+MSE = np.mean((y_test.ravel()-temp_pred.ravel())**2)
+
